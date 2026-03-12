a0dfe4a2d0ebca2a01326bd0ef97c3d47912648e
Wellington <waguiarbjj@hotmail.com>
2026-03-12 14:42:12 -0300

    refactor: standardize logs in English

--- main.py
+++ main.py
@@ -5,18 +5,25 @@ from scripts.silver import process_silver
 from scripts.gold import process_gold
 
 def main():
-    # Inicializa o Spark (Preferencial para o teste) 
-    spark = SparkSession.builder.appName("Bees_Case").getOrCreate()
-    print("--- Pipeline Iniciado ---")
+    # Spark Session initialization
+    spark = SparkSession.builder \
+        .appName("BeesBreweryPipeline") \
+        .getOrCreate()
+
+    print("--- Pipeline Started ---")
+
     try:
-        # Orquestração das camadas [cite: 8]
+        # Layer orchestration
         bronze_file = fetch_api_data() 
         process_silver(spark, bronze_file)
         process_gold(spark, "data/silver/breweries_partitioned")
-        print("--- Pipeline Finalizado com Sucesso! ---")
+
+        print("--- Pipeline Finished Successfully! ---")
+
     except Exception as e:
-        print(f"Falha na execução: {e}") # [cite: 13]
+        print(f"Execution Failure: {e}")
         sys.exit(1)
+
     finally:
         spark.stop()
 
